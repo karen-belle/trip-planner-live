76b3227176d0f7d9ce59aadc5bab778c8808b90a
Karen Abramowitz <karenabramowitz@gmail.com>
2016-05-12 18:33:37 -0400

    Pretty good

---
+++ public/js/options.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    for (var i = 0; i < hotels.length; i++) {
+        var hotel = hotels[i];
+        $(`<option name="hotel" value="${hotel.id}"> ${hotel.name} </option>`).appendTo('#hotel-choices');     
+    }
+
+     for (var i = 0; i < restaurants.length; i++) {
+        var restaurant = restaurants[i];
+        $(`<option name="restaurant" value="${restaurant.id}"> ${restaurant.name} </option>`).appendTo('#restaurant-choices');     
+    }
+
+     for (var i = 0; i < activities.length; i++) {
+        var activity = activities[i];
+        $(`<option name="activity" value="${activity.id}"> ${activity.name} </option>`).appendTo('#activity-choices');     
+    }
+
+    $('.btn.hotelbtn').on('click', function(){
+    	var selection = $(this).siblings('select').find('option:selected').val();
+		
+		var selectedHotel = hotels.filter(function(hotel, index){
+			return hotel.id === +selection;
+		});
+
+    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedHotel[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+
+    	$(itinItem).appendTo('.hotel-list');
+    	var placeId = selectedHotel[0].placeId
+    	
+    	drawMarker('hotel', places[placeId].location);
+    });
+
+    $('.btn.restaurantbtn').on('click', function(){
+    	var selection = $(this).siblings('select').find('option:selected').val();
+		
+		var selectedRestaurant = restaurants.filter(function(restaurant, index){
+			return restaurant.id === +selection;
+		});
+
+    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedRestaurant[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+
+    	$(itinItem).appendTo('.restaurant-list');
+    });
+
+    $('.btn.activitybtn').on('click', function(){
+    	var selection = $(this).siblings('select').find('option:selected').val();
+		
+		var selectedActivity = activities.filter(function(activity, index){
+			return activity.id === +selection;
+		});
+
+    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedActivity[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+
+    	$(itinItem).appendTo('.activity-list');
+    });
+
+
+})
---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var router = require('express').Router();
+var Promise = require('bluebird');
+// var models = require('../models')
+var Hotel = require('../models/hotel');
+var Restaurant = require('../models/restaurant');
+var Activity = require('../models/activity');
+var Place = require('../models/place');
+
+router.get('/', function (req, res, next) {
+  Promise.all([
+    Hotel.findAll(),
+    Restaurant.findAll(),
+    Activity.findAll(),
+    Place.findAll()
+  ])
+  .spread(function (hotels, restaurants, activities, places) {
+    res.render('index', {
+      hotels: hotels,
+      restaurants: restaurants,
+      activities: activities,
+      places: places
+
+    });
+  })
+  .catch(next);
+});
+
+module.exports = router;
