d95613209b7c321d7e155fb1e162914c81c3e9ad
Karen Abramowitz <karenabramowitz@gmail.com>
2016-05-13 10:38:21 -0400

    markers drawn

--- public/js/options.js
+++ public/js/options.js
@@ -1,4 +1,11 @@
+
+
+
 $(document).ready(function() {
+    // $.getScript('./main.js', function (){console.log(initializeMap)});
+
+    var curMap = initializeMap();
+
     for (var i = 0; i < hotels.length; i++) {
         var hotel = hotels[i];
         $(`<option name="hotel" value="${hotel.id}"> ${hotel.name} </option>`).appendTo('#hotel-choices');     
@@ -15,42 +22,42 @@ $(document).ready(function() {
     }
 
     $('.btn.hotelbtn').on('click', function(){
-    	var selection = $(this).siblings('select').find('option:selected').val();
-		
-		var selectedHotel = hotels.filter(function(hotel, index){
-			return hotel.id === +selection;
-		});
+        var selection = $(this).siblings('select').find('option:selected').val();
+        
+        var selectedHotel = hotels.filter(function(hotel, index){
+            return hotel.id === +selection;
+        });
 
-    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedHotel[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+        var itinItem = `<div class="itinerary-item"><span class="title">${selectedHotel[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
 
-    	$(itinItem).appendTo('.hotel-list');
-    	var placeId = selectedHotel[0].placeId
-    	
-    	drawMarker('hotel', places[placeId].location);
+        $(itinItem).appendTo('.hotel-list');
+        var placeId = selectedHotel[0].placeId
+        
+        drawMarker(curMap, 'hotel', places[placeId].location);
     });
 
     $('.btn.restaurantbtn').on('click', function(){
-    	var selection = $(this).siblings('select').find('option:selected').val();
-		
-		var selectedRestaurant = restaurants.filter(function(restaurant, index){
-			return restaurant.id === +selection;
-		});
+        var selection = $(this).siblings('select').find('option:selected').val();
+        
+        var selectedRestaurant = restaurants.filter(function(restaurant, index){
+            return restaurant.id === +selection;
+        });
 
-    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedRestaurant[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+        var itinItem = `<div class="itinerary-item"><span class="title">${selectedRestaurant[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
 
-    	$(itinItem).appendTo('.restaurant-list');
+        $(itinItem).appendTo('.restaurant-list');
     });
 
     $('.btn.activitybtn').on('click', function(){
-    	var selection = $(this).siblings('select').find('option:selected').val();
-		
-		var selectedActivity = activities.filter(function(activity, index){
-			return activity.id === +selection;
-		});
+        var selection = $(this).siblings('select').find('option:selected').val();
+        
+        var selectedActivity = activities.filter(function(activity, index){
+            return activity.id === +selection;
+        });
 
-    	var itinItem = `<div class="itinerary-item"><span class="title">${selectedActivity[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
+        var itinItem = `<div class="itinerary-item"><span class="title">${selectedActivity[0].name}</span> <button class="btn btn-xs btn-danger remove btn-circle">x</button></div>`
 
-    	$(itinItem).appendTo('.activity-list');
+        $(itinItem).appendTo('.activity-list');
     });
 
 
